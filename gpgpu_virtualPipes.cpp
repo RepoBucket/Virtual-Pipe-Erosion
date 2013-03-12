ee74d08901978d86a8fcbb39a688df37c957b763
fluffyterran@gmail.com <fluffyterran@gmail.com>
2013-03-12 18:47:22 +0900

    Changes referencing a tutorial.

--- gpgpu_virtualPipes.cpp
+++ gpgpu_virtualPipes.cpp
@@ -1,3 +1,83 @@
+#include "gpgpu_virtualPipes.h"
+#include "parse.h"
+
+const char* oclErrorString(cl_int error)
+{
+    static const char* errorString[] = {
+        "CL_SUCCESS",
+        "CL_DEVICE_NOT_FOUND",
+        "CL_DEVICE_NOT_AVAILABLE",
+        "CL_COMPILER_NOT_AVAILABLE",
+        "CL_MEM_OBJECT_ALLOCATION_FAILURE",
+        "CL_OUT_OF_RESOURCES",
+        "CL_OUT_OF_HOST_MEMORY",
+        "CL_PROFILING_INFO_NOT_AVAILABLE",
+        "CL_MEM_COPY_OVERLAP",
+        "CL_IMAGE_FORMAT_MISMATCH",
+        "CL_IMAGE_FORMAT_NOT_SUPPORTED",
+        "CL_BUILD_PROGRAM_FAILURE",
+        "CL_MAP_FAILURE",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "CL_INVALID_VALUE",
+        "CL_INVALID_DEVICE_TYPE",
+        "CL_INVALID_PLATFORM",
+        "CL_INVALID_DEVICE",
+        "CL_INVALID_CONTEXT",
+        "CL_INVALID_QUEUE_PROPERTIES",
+        "CL_INVALID_COMMAND_QUEUE",
+        "CL_INVALID_HOST_PTR",
+        "CL_INVALID_MEM_OBJECT",
+        "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR",
+        "CL_INVALID_IMAGE_SIZE",
+        "CL_INVALID_SAMPLER",
+        "CL_INVALID_BINARY",
+        "CL_INVALID_BUILD_OPTIONS",
+        "CL_INVALID_PROGRAM",
+        "CL_INVALID_PROGRAM_EXECUTABLE",
+        "CL_INVALID_KERNEL_NAME",
+        "CL_INVALID_KERNEL_DEFINITION",
+        "CL_INVALID_KERNEL",
+        "CL_INVALID_ARG_INDEX",
+        "CL_INVALID_ARG_VALUE",
+        "CL_INVALID_ARG_SIZE",
+        "CL_INVALID_KERNEL_ARGS",
+        "CL_INVALID_WORK_DIMENSION",
+        "CL_INVALID_WORK_GROUP_SIZE",
+        "CL_INVALID_WORK_ITEM_SIZE",
+        "CL_INVALID_GLOBAL_OFFSET",
+        "CL_INVALID_EVENT_WAIT_LIST",
+        "CL_INVALID_EVENT",
+        "CL_INVALID_OPERATION",
+        "CL_INVALID_GL_OBJECT",
+        "CL_INVALID_BUFFER_SIZE",
+        "CL_INVALID_MIP_LEVEL",
+        "CL_INVALID_GLOBAL_WORK_SIZE",
+    };
+
+    const int errorCount = sizeof(errorString) / sizeof(errorString[0]);
+
+    const int index = -error;
+
+    return (index >= 0 && index < errorCount) ? errorString[index] : "";
+
+}
+
 void gpgpu_VirtualPipe::startup()
   {
   cl_int whatHappened;
@@ -17,12 +97,10 @@ void gpgpu_VirtualPipe::startup()
   std::vector<cl_int> in_intArray(1024, 2);
 
   cl::Context* context = new cl::Context(devices->at(0), NULL, NULL);
-  cl::Buffer* outbuffer = new cl::Buffer(*context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, sizeof(cl_int) * 1024, &out_intArray[0], &whatHappened);
+  cl::Buffer* outbuffer = new cl::Buffer(*context, CL_MEM_READ_ONLY, sizeof(cl_int) * 1024, NULL, &whatHappened);
   std::cout << "What happened with creating outbuffer: " << oclErrorString(whatHappened) << std::endl;
   cl::Buffer* inbuffer = new cl::Buffer(*context, CL_MEM_WRITE_ONLY, sizeof(cl_int) * 1024, NULL, &whatHappened);
   std::cout << "What happened with creating inbuffer: " << oclErrorString(whatHappened) << std::endl;
-
-  cl_mem outbuffer = 
 
   std::vector<cl::Program>* programs = new std::vector<cl::Program>();
   std::vector<cl::Kernel>* kernels = new std::vector<cl::Kernel>();
@@ -40,8 +118,8 @@ void gpgpu_VirtualPipe::startup()
   whatHappened = (*programs)[0].createKernels(kernels);
   std::cout << "What happened with creating a kernel: " << oclErrorString(whatHappened) << std::endl;
 
-  (*kernels)[0].setArg(0, sizeof(cl_int) * 1024, outbuffer);
-  (*kernels)[0].setArg(1, sizeof(cl_int) * 1024, inbuffer);
+  (*kernels)[0].setArg(0, outbuffer);
+  (*kernels)[0].setArg(1, inbuffer);
   
   const size_t global_item_size = 1024;
   const size_t local_item_size = 64;
@@ -53,10 +131,13 @@ void gpgpu_VirtualPipe::startup()
   cl::CommandQueue commandQueue((*context), (*devices)[0]);
   whatHappened = commandQueue.enqueueWriteBuffer(*outbuffer, true, 0, sizeof(cl_int) * 1024, &out_intArray[0]);
   std::cout << "What happened with writing buffer to gpu: " << oclErrorString(whatHappened) << std::endl;
+  commandQueue.finish();
   commandQueue.enqueueNDRangeKernel(kernels->at(0), offset, globalRange, localRange);
   std::cout << "What happened with excecuting kernel: " << oclErrorString(whatHappened) << std::endl;
+  commandQueue.finish();
   commandQueue.enqueueReadBuffer(*inbuffer, true, 0, sizeof(cl_int) * 1024, &(in_intArray)[0]);
   std::cout << "What happened with reading buffer from gpu: " << oclErrorString(whatHappened) << std::endl;
+  commandQueue.finish();
 
   for (int counter = 0; counter < 1024; counter++)
     std::cout << "Position " << counter << " is " << in_intArray[counter] << std::endl;
